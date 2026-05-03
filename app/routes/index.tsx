e601feaafa16b7ae66dbb1ab2131b94d29aa739b
sugawara-kouki <kou.720131@gmail.com>
2026-05-03 16:28:35 +0900

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createRoute } from 'honox/factory'
+
+export default createRoute((c) => {
+  return c.render(
+    <div class="py-8 text-center">
+      <h1>Hello! from sugach4n-dev</h1>
+    </div>
+  )
+})
